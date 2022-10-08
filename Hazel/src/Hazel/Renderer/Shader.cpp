a6f90a88d77086fe5de2a9836988bdcc2947ba9c
RB <1024906432@qq.com>
2022-10-08 12:15:16 +0800

    first shader

---
+++ Hazel/src/Hazel/Renderer/Shader.cpp
@@ -0,0 +1,127 @@
+#include "hzpch.h"
+#include "Shader.h"
+
+#include <glad/glad.h>
+
+namespace Hazel {
+
+	Shader::Shader(const std::string& vertexSrc, const std::string& fragmentSrc)
+	{
+		// 创建空顶点着色器句柄
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		// 将顶点着色器源代码发送到 GL
+		// Note that std::string's .c_str is NULL character terminated.
+		const GLchar* source = vertexSrc.c_str();
+		glShaderSource(vertexShader, 1, &source, 0);
+
+		//编译顶点着色器
+		glCompileShader(vertexShader);
+
+		GLint isCompiled = 0;
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the shader anymore.
+			glDeleteShader(vertexShader);
+
+			HZ_CORE_ERROR("{0}", infoLog.data());
+			HZ_CORE_ASSERT(false, "Vertex shader compilation failure!");
+			return;
+		}
+
+		//  创建空片元着色器句柄
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		// 将片元着色器源代码发送到 GL
+		// Note that std::string's .c_str is NULL character terminated.
+		source = fragmentSrc.c_str();
+		glShaderSource(fragmentShader, 1, &source, 0);
+
+		// Compile the fragment shader
+		glCompileShader(fragmentShader);
+
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the shader anymore.
+			glDeleteShader(fragmentShader);
+			// Either of them. Don't leak shaders.
+			glDeleteShader(vertexShader);
+
+			HZ_CORE_ERROR("{0}", infoLog.data());
+			HZ_CORE_ASSERT(false, "Fragment shader compilation failure!");
+			return;
+		}
+
+		//顶点着色器和片段着色器已成功编译。
+		//现在是时候将它们链接到一个程序中了。
+		//获取程序对象。
+		m_RendererID = glCreateProgram();
+		GLuint program = m_RendererID;
+
+		// Attach our shaders to our program
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
+
+		// Link our program
+		glLinkProgram(program);
+
+		// Note the different functions here: glGetProgram* instead of glGetShader*.
+		GLint isLinked = 0;
+		glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
+		if (isLinked == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+			// The maxLength includes the NULL character
+			std::vector<GLchar> infoLog(maxLength);
+			glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+
+			// We don't need the program anymore.
+			glDeleteProgram(program);
+			// Don't leak shaders either.
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+
+			HZ_CORE_ERROR("{0}", infoLog.data());
+			HZ_CORE_ASSERT(false, "Shader link failure!");
+			return;
+		}
+
+		// 始终在链接成功后分离着色器。
+		glDetachShader(program, vertexShader);
+		glDetachShader(program, fragmentShader);
+	}
+
+	Shader::~Shader()
+	{
+		glDeleteProgram(m_RendererID);
+	}
+
+	void Shader::Bind() const
+	{
+		glUseProgram(m_RendererID);
+	}
+
+	void Shader::Unbind() const
+	{
+		glUseProgram(0);
+	}
+
+}
