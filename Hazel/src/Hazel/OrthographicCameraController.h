2e8d0883ed16aea592535056c564becf8cfcf333
RB <1024906432@qq.com>
2022-10-12 15:13:06 +0800

    add camera controller

---
+++ Hazel/src/Hazel/OrthographicCameraController.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+
+#include "Hazel/Core/Timestep.h"
+
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+
+namespace Hazel
+{
+	class OrthographicCameraController
+	{
+	public:
+		OrthographicCameraController(float aspectRatio, bool rotation = false);
+		void OnUpdate(Timestep ts);//根据移动速度和旋转速度 控制相机变换
+		void OnEvent(Event& e);//绑定按键回调函数 
+
+		OrthographicCamera& GetCamera() { return m_Camera; }
+		const OrthographicCamera& GetCamera() const { return m_Camera; }
+
+	private:
+		bool OnMouseScrolled(MouseScrolledEvent& e);
+		bool OnWindowResized(WindowResizeEvent& e);
+
+	private:
+
+	private:
+		float m_AspectRatio;
+		float m_ZoomLevel = 1.0f;
+		OrthographicCamera m_Camera;
+		bool m_Rotation;
+
+		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
+		float m_CameraRotation = 0.0f;
+		float m_CameraTranslationSpeed = 5.0f, m_CameraRotationSpeed = 180.0f;
+	};
+}
--- Sandbox/SandboxApp.cpp
+++ Sandbox/SandboxApp.cpp
@@ -8,7 +8,7 @@ class ExampleLayer : public Hazel::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example"), m_Camera(-2.f, 2.f, -3.f, 1.f), m_CameraPosition(1.0f)
+		: Layer("Example"), m_CameraController(1280.0f / 720.0f, true)
 	{
 		m_VertexArray.reset(Hazel::VertexArray::Create());
 
@@ -126,28 +126,14 @@ public:
 
 	void OnUpdate(Hazel::Timestep ts) override
 	{
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_LEFT))
-			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
-		else if (Hazel::Input::IsKeyPressed(HZ_KEY_RIGHT))
-			m_CameraPosition.x += m_CameraMoveSpeed * ts;
-
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_UP))
-			m_CameraPosition.y += m_CameraMoveSpeed * ts;
-		else if (Hazel::Input::IsKeyPressed(HZ_KEY_DOWN))
-			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
-
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_A))
-			m_CameraRotation += m_CameraRotationSpeed * ts;
-		if (Hazel::Input::IsKeyPressed(HZ_KEY_D))
-			m_CameraRotation -= m_CameraRotationSpeed * ts;
+		// Update
+		m_CameraController.OnUpdate(ts);
 
 		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Hazel::RenderCommand::Clear();
 
-		m_Camera.SetPosition(m_CameraPosition);
-		m_Camera.SetRotation(m_CameraRotation);
 
-		Hazel::Renderer::BeginScene(m_Camera);
+		Hazel::Renderer::BeginScene(m_CameraController.GetCamera());
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 		std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
@@ -178,9 +164,9 @@ public:
 		ImGui::End();
 	}
 
-	void OnEvent(Hazel::Event& event) override
+	void OnEvent(Hazel::Event& e) override
 	{
-
+		m_CameraController.OnEvent(e);
 	}
 
 	private:
@@ -191,12 +177,7 @@ public:
 		Hazel::Ref<Hazel::Shader> m_FlatColorShader;
 		Hazel::Ref<Hazel::VertexArray> m_SquareVA;
 		Hazel::Ref<Hazel::Texture2D> m_Texture, m_ChernoLogoTexture;
-		Hazel::OrthographicCamera m_Camera;
-		glm::vec3 m_CameraPosition;
-		float m_CameraMoveSpeed = 5.0f;
-
-		float m_CameraRotation = 0.0f;
-		float m_CameraRotationSpeed = 180.0f;
+		Hazel::OrthographicCameraController m_CameraController;
 		glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
